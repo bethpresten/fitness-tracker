54509a1b495b2d3f82a584ccf0f3a79bdf5928af
Beth Presten <bethpresten@gmail.com>
2021-01-05 18:03:02 -0500

    updated all the api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,79 +1,50 @@
-const db = require("../models");
-const express = require("express");
-const path = require("path");
+const db = require("../models/index");
 
 module.exports = function (app) {
     app.get("/api/config", (req, res) => {
         res.json({
             success: true,
+        }).catch(err => {
+            console.log(err)
         });
-        // .catch((err)) => {
-        //     console.log(err);
-        // };
     });
 
     app.get("/api/workouts", (req, res) => {
-        db.Workout.find().then((workouts) => {
-            res.json(workouts);
-            console.log(workouts);
-        });
-        // .catch((err)) => {
-        //     console.log(err);
-        // };
-
-        // delete method: getLastWorkout (see api.js)
-
-
-        // update method that will be addExercise    (see api.js)
-
-
-        //post method that will be createExercise (see api.js)
-
-
-        // getWorkoutsInRange will be a GET method
+        db.Workout.find({})
+            .then(workouts => {
+                res.json(workouts)
+            }).catch(err => {
+                console.log(err)
+            });
     });
 
+    app.post("/api/workouts", ({ body }, res) => {
+        db.Workout.create(body)
+            .then(db => {
+                res.json(db);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    });
 
     app.get("/api/workouts/range", (req, res) => {
-        db.Workout.find().then((workouts) => {
-            res.json(workouts);
-        });
-        // .catch((err)) => {
-        //     console.log(err);
-        // };
-    });
-
-    app.get("/api/workouts/:id", (req, res) => {
-        db.Workout.findById(req.params.id).then((workouts) => {
-            res.json(workouts);
-        });
-        // .catch(err) => {
-        //     console.log(err);
-        // };
-    });
-
-    app.post("/api/workouts", (req, res) => {
-        db.Workout.create(req.body).then((workouts) => {
-            res.json(workouts);
-        })
-        // .catch((err)) => {
-        //     console.log(err);
-        // };
+        db.Workout.find({})
+            .then(workouts => {
+                res.json(workouts)
+            }).catch((err) => {
+                console.log(err)
+            });
     });
 
     app.put("/api/workouts/:id", (req, res) => {
-        db.Workout.findByIdAndUpdate(req.params.id, req.body, { new: true }).then(
-            (workouts) => {
-                res.json(workouts);
-            });
-        //     .catch(err) => {
-        //     console.log(err);
-        // }
-    });
-
-    app.delete("/api/workouts/:id", (req, res) => {
-        db.Workout.findByIdAndDelete(req.params.id).then((result) => {
-            res.json(result);
+        db.Workout.findOneAndUpdate(
+            { _id: req.params.id },
+            { $push: { exercises: req.body } }
+        ).then((workouts) => {
+            res.json(workouts);
+        }).catch((err) => {
+            console.log(err);
         });
     });
-};
+}
--- server.js
+++ server.js
@@ -3,14 +3,15 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const logger = require("morgan");
-// 2. Create an instance of Express
+const Workout = require("./models/workout");
+
 const app = express();
-// 3. Set the PORT
+
 const PORT = process.env.PORT || 8080;
 
-// const db = require("./models");
+
 app.use(logger("dev"));
-// 5. Add middleware
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
